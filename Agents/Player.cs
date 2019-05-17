9a86939bbfd5a98c99675c97a708196fc53263f6
RodrigoMarques23 <rodrigomarques6923@gmail.com>
2019-05-17 16:26:36 +0100

    class player + array agent

--- Agents/Player.cs
+++ Agents/Player.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace Agents
 {
-    class Player
+    public class Player : Agent
     {
+        public Player(string name) : base(name) { }
+        public override char Move()
+        {
+            Console.Write("Insira direção(NSWE)");
+            return Console.ReadLine().ToUpper()[0];
+        }
+
+        public override string ToString() => $"Im a player and my name is {Name}";
     }
 }
